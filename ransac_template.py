697f286c333191781dd2611bfd1d9d6564219d00
jared12222 <jared12222@gmail.com>
2020-11-05 18:37:55 -0500

    Change fitting to only choose 3 random point from set

--- ransac_template.py
+++ ransac_template.py
@@ -16,58 +16,73 @@ def main():
     utils.view_pc([pc])
 
     #Fit a plane to the data using ransac
-    iteration = 2000
-    N = 150
+    iteration = 500
+    N = 120
     epson = 0.07
 
     N_to_fit = 3
     N_sample = len(pc)
 
-    picked = set()
     error_best = numpy.Infinity
     model = None
 
     for i in range(iteration):
-        picked.clear()
+        picked=[]
         pts = []
         while len(picked) < N_to_fit:
             n = numpy.random.randint(0,N_sample)
             if n not in picked:
-                picked.add(n)
+                picked.append(n)
                 pts.append(pc[n])
         
-        plane_org, plane_normal = plane_model(pts)
+        plane_org = pts[0]
+        plane_normal = numpy.cross(numpy.reshape(numpy.array(pts[1]-pts[0]),(3,)),numpy.reshape(numpy.array(pts[2]-pts[0]),(3,)))
 
         for j in range(N_sample):
             if j not in picked:
                 if error_plane(pc[j],[plane_org,plane_normal]) <= epson:
-                    picked.add(j)
                     pts.append(pc[j])
-        if len(picked) >= N:
-            plane_org, plane_normal = plane_model(pts)
+        
+
+        if len(pts) >= N:
+            picked = []
+            pt = []
+            while len(pt) < N_to_fit:
+                n = numpy.random.randint(0,len(pts))
+                if n not in picked :
+                    picked.append(n)
+                    pt.append(pts[n])
+
+            # plane_org, plane_normal = plane_model(pts)
+            plane_org = pts[0]
+            plane_normal = numpy.cross(numpy.reshape(numpy.array(pts[1]-pts[0]),(3,)),numpy.reshape(numpy.array(pts[2]-pts[0]),(3,)))
+            plane_normal = numpy.reshape(numpy.matrix(plane_normal),(3,1))
+
             error_new = 0
             for j in range(len(pts)):
                 error_new = error_new + error_plane(pts[j],[plane_org, plane_normal])
+            error_new = error_new/len(pts)
             if error_new < error_best:
                 error_best = error_new
                 model = [plane_org, plane_normal]
 
     #Show the resulting point cloud
-    inlier  = []
-    outlier = []
-    
-    for j in range(N_sample):
-        if error_plane(pc[j],model) <= epson:
-            inlier.append(pc[j])
-        else:
-            outlier.append(pc[j])
-    fig = utils.view_pc([inlier],color='r')
-    fig = utils.view_pc([outlier],fig,color='b')
-
-    #Draw the fitted plane
     if model == None:
         print "model not found"
+    
     else:
+        inlier  = []
+        outlier = []
+        
+        for j in range(N_sample):
+            if error_plane(pc[j],model) <= epson:
+                inlier.append(pc[j])
+            else:
+                outlier.append(pc[j])
+        fig = utils.view_pc([inlier],color='r')
+        fig = utils.view_pc([outlier],fig,color='b')
+
+    #Draw the fitted plane
         fig = utils.draw_plane(fig,model[1],model[0],color=[0,1,0,0.3])
         print model[1]
         print model[0]
